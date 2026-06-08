fcf4d8503b140a08141a8116de62081bdb4ed107
hrnooop <harisnooop@gmail.com>
2026-06-08 11:28:50 +0700

    berlatih membuat konverter umur manusia ke umur anjing

---
+++ humanAgeToDog.js
@@ -0,0 +1,11 @@
+let myAge = 99 ; // initial age of human
+let earlyYears = 2 ; // nilai bisa diganti
+earlyYears *= 10.5 ;
+myAge -= 2 ;
+let laterYears = myAge ;
+laterYears *= 4 ;
+let myAgeInDogYears = laterYears + earlyYears ;
+let myName = "WOWO KONTOL";
+myName = myName.toLowerCase();
+console.log(myName);
+console.log(`My name is ${myName}. i am ${myAge} years old ini human years, which is ${myAgeInDogYears} years old in dog years`) ;
